d2dbdb4a917a101c16da0644330c1fad521afcb7
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-03-18 09:08:24 -0700

    Adding login capabilities.

---
+++ famnet/lib/first_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:sign_in_flutter/login_page.dart';
+import 'package:sign_in_flutter/sign_in.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.blue[100])
+    );
+  }
+}
