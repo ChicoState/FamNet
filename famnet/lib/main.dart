0d6e24a19c7e76f7e2481322435dd35854cf51d1
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-03-18 09:08:48 -0700

    Changing splash screen.

--- famnet/lib/main.dart
+++ famnet/lib/main.dart
@@ -1,19 +1,21 @@
 //Adapted from "Making a To-do App with Flutter" https://medium.com/the-web-tub/making-a-todo-app-with-flutter-5c63dab88190
 import 'package:flutter/material.dart';
 import 'todo.dart';
+import 'login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 void main() => runApp(new FamNetApp());
 
 class FamNetApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'To-do List',
-        theme: ThemeData.light(),
-        darkTheme: ThemeData.dark(),
-        home: new TodoList()
+    return MaterialApp(
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
     );
   }
 }
