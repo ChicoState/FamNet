4f4bf5dc5775fa26e6b0f0c5e1bf2a148de77720
Kyle Kirk <ktkirk@jaguar.csuchico.edu>
2020-03-18 09:13:25 -0700

    Sign in dependencies updated.

--- famnet/lib/first_screen.dart
+++ famnet/lib/first_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sign_in_flutter/login_page.dart';
-import 'package:sign_in_flutter/sign_in.dart';
+import 'login_page.dart';
+import 'sign_in.dart';
 
 class FirstScreen extends StatelessWidget {
   @override
