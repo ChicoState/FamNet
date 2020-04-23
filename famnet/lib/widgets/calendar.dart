eabac3b6f88fe00b3d272c10f61b4fc74925011f
Joseph Macam <jdmacam@gmail.com>
2020-04-23 12:15:58 -0700

    Added interactable calendar widget

--- famnet/lib/widgets/calendar.dart
+++ famnet/lib/widgets/calendar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatefulWidget {
   @override
@@ -9,8 +10,14 @@ class Calendar extends StatefulWidget {
 }
 
 class CalendarState extends State<Calendar> {
+  CalendarController _controller;
 
   @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
@@ -23,6 +30,14 @@ class CalendarState extends State<Calendar> {
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(calendarController: _controller,)
+          ],
+        )
+      )
 
     );
   }
